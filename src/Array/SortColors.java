3efa28c82de3c9771ec05fe057751ff4af1ba724
nick <ndikanskiy@gmail.com>
2022-12-29 23:21:51 +0400

    efficient solution for task called "Sort Colors" has been added

--- src/Array/SortColors.java
+++ src/Array/SortColors.java
@@ -10,23 +10,50 @@ public class SortColors {
 
         System.out.println(Arrays.toString(colors));
 
-        sortColors(colors);
+        heapSort(colors);
 
         System.out.println(Arrays.toString(colors));
 
     }
 
-    // O(N^2) Time
-    // O(1) Space
-    public static void sortColors(int[] colors) {
-        for (int i = 0; i < colors.length; i++) {
-            for (int j = i + 1; j < colors.length; j++) {
-                if (colors[j] < colors[i]) {
-                    int temp = colors[i];
-                    colors[i] = colors[j];
-                    colors[j] = temp;
-                }
-            }
+    // Time (N log(N))
+    // Space (N log(N))
+    public static void heapSort(int[] arr) {
+        createHeap(arr);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void createHeap(int[] arr) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, arr.length, i);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private static void heapify(int[] arr, int heapSize, int i) {
+        int largest = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < heapSize && arr[leftChild] > arr[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < heapSize && arr[rightChild] > arr[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, heapSize, largest);
         }
     }
 
